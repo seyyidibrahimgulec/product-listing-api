80e645bb4f8ac2f6f8d3b47aeb2fb525bb104693
Seyyid Ibrahim Gulec <seyyidibrahimgulec@gmail.com>
2022-09-11 12:50:12 +0300

    Refactor product handlers

--- app/handlers/product.go
+++ app/handlers/product.go
@@ -10,21 +10,49 @@ import (
 	"github.com/seyyidibrahimgulec/product-listing/app/models"
 )
 
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func getProductRespondError(w http.ResponseWriter, r *http.Request) *models.Product {
+	vars := mux.Vars(r)
+	productId := vars["productId"]
+	id, err := strconv.ParseInt(productId, 0, 0)
+	if err != nil {
+		logError(err)
+		createJSONResponse(w, http.StatusBadRequest, []byte{})
+		return nil
+	}
+	product, err := models.GetProductById(id)
+	if err != nil {
+		logError(err)
+		createJSONResponse(w, http.StatusNotFound, []byte{})
+		w.WriteHeader(http.StatusNotFound)
+		return nil
+	}
+	return product
+}
+
+func createJSONResponse(w http.ResponseWriter, status_code int, response []byte) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status_code)
+	w.Write(response)
+}
+
 func listProducts(w http.ResponseWriter, r *http.Request) {
 	products := models.GetAllProducts()
-	res, _ := json.Marshal(products)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	res, err := json.Marshal(products)
+	logError(err)
+	createJSONResponse(w, http.StatusOK, res)
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
 	product := models.Product{}
 	decoder := json.NewDecoder(r.Body)
-	w.Header().Set("Content-Type", "application/json")
 	if err := decoder.Decode(&product); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
+		createJSONResponse(w, http.StatusBadRequest, []byte(err.Error()))
 		return
 	}
 	defer r.Body.Close()
@@ -32,72 +60,46 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 	p, err := product.CreateProduct()
 
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		createJSONResponse(w, http.StatusInternalServerError, []byte{})
 		log.Println([]byte(err.Error()))
 	} else {
-		res, _ := json.Marshal(p)
-		w.WriteHeader(http.StatusCreated)
-		w.Write(res)
+		res, err := json.Marshal(p)
+		logError(err)
+		createJSONResponse(w, http.StatusCreated, res)
 	}
 }
 
 func retrieveProduct(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	vars := mux.Vars(r)
-	productId := vars["productId"]
-	id, err := strconv.ParseInt(productId, 0, 0)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
+	product := getProductRespondError(w, r)
+	if product == nil {
 		return
 	}
-	productDetails, err := models.GetProductById(id)
+	res, err := json.Marshal(product)
 	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusNotFound)
+		logError(err)
+		createJSONResponse(w, http.StatusInternalServerError, []byte{})
 		return
 	}
-	res, err := json.Marshal(productDetails)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	createJSONResponse(w, http.StatusOK, res)
 }
 
 func updateProduct(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	productId := vars["productId"]
-	id, err := strconv.ParseInt(productId, 0, 0)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	product, err := models.GetProductById(id)
-	if err != nil {
-		log.Println(err)
-		w.WriteHeader(http.StatusNotFound)
+	product := getProductRespondError(w, r)
+	if product == nil {
 		return
 	}
 	decoder := json.NewDecoder(r.Body)
-	w.Header().Set("Content-Type", "application/json")
 	if err := decoder.Decode(&product); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
+		createJSONResponse(w, http.StatusBadRequest, []byte(err.Error()))
 		return
 	}
 	defer r.Body.Close()
 
-	product, err = product.UpdateProduct()
-	if err != nil {
-		log.Println(err)
-	}
-	res, _ := json.Marshal(product)
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
+	product, err := product.UpdateProduct()
+	logError(err)
+	res, err := json.Marshal(product)
+	logError(err)
+	createJSONResponse(w, http.StatusOK, res)
 }
 
 var RegisterProductRoutes = func(router *mux.Router) {
